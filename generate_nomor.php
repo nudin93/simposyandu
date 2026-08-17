b5e2694eb79e225274b40cbbce1030c14a9d287a
Zainudin larau <92310027+nudin93@users.noreply.github.com>
2026-08-17 10:31:01 +0800

    Delete generate_nomor.php

--- generate_nomor.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/init.php';
-requireLogin();
-header('Content-Type: application/json');
-$type = $_GET['type'] ?? '';
-$map = ['balita' => ['BLT', 'balita'], 'ibu_hamil' => ['IBH', 'ibu_hamil'], 'lansia' => ['LNS', 'lansia']];
-if (!isset($map[$type])) { echo json_encode(['error' => 'Invalid type']); exit; }
-[$prefix, $table] = $map[$type];
-echo json_encode(['nomor' => generateNomorPeserta($prefix, $table)]);
